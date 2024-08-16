e4a9f1f838d71babd09f3171d140d5b3223db7c9
VitaliySaburdo <vsaburdo@gmail.com>
2024-08-16 19:10:09 +0300

    1

--- src/components/MapComponent/MapComponent.tsx
+++ src/components/MapComponent/MapComponent.tsx
@@ -1,5 +1,21 @@
+import { useEffect } from 'react';
+import L from 'leaflet';
+
 export const MapComponent = () => {
-  return (
-    <div>MapComponent</div>
-  )
+  useEffect(() => {
+    const map = L.map('map', {
+      center: [51.505, -0.09],
+      zoom: 13,
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    return () => {
+      map.remove();
+    };
+  }, []);
+
+  return <div id="map" style={{ height: '300px', width: '100%' }}></div>;
 }
